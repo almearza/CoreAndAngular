77a7460a6a31c904fd0c1d99318ea0f320460813
fawaz almogadem <fawazalmogadem@hotmail.com>
2021-05-06 00:17:28 +0300

    some changes sec 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
+import { FileUploader } from 'ng2-file-upload';
 @NgModule({
   declarations: [
     AppComponent,
@@ -55,7 +56,8 @@ import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
     }),
     SharedModule,
     NgxGalleryModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FileUploader,
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true},
--- client/src/app/photo-editor/photo-editor.component.ts
+++ client/src/app/photo-editor/photo-editor.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { url } from 'node:inspector';
+import { take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,9 +14,39 @@ import { Member } from '../_models/member';
 })
 export class PhotoEditorComponent implements OnInit {
 @Input() member:Member;
-  constructor() { }
 
+uploader:FileUploader;
+hasBaseDropZoneOver=false;
+baseUrl=environment.baseUrl;
+user:User;
+  constructor(private accountService:AccountService) { 
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
+      this.user=user;
+    })
+  }
+  fileOverBase(e:any){
+  this.hasBaseDropZoneOver=e;
+}
   ngOnInit(): void {
   }
-
+initializeUploader(){
+  this.uploader=new FileUploader({
+    url:this.baseUrl+'users/add-photo',
+    authToken:this.user.token,
+    allowedFileType:['image'],
+    removeAfterUpload:true,
+    autoUpload:false,
+    isHTML5:true,
+    maxFileSize:10*1024*1024
+  });
+  this.uploader.onAfterAddingFile=(file)=>{
+    file.withCredentials=false;
+  }
+  this.uploader.onSuccessItem=(item,response,status,headers)=>{
+    if(response){
+      const photo = JSON.parse(response);
+      this.member.photos.push(photo);
+    }
+  }
+}
 }
