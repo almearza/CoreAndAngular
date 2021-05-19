9c36cb64f51a5a78325b20c491404365bdc1204a
fawaz almogadem <fawazalmogadem@hotmail.com>
2021-05-19 23:14:43 +0300

    end of vid 9

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
+import { User } from '../_models/user';
 import { getPaginattedHeaders, getPaginattedResult } from './pagination';
 
 @Injectable({
@@ -10,6 +13,12 @@ import { getPaginattedHeaders, getPaginattedResult } from './pagination';
 })
 export class MessageService {
   baseUrl = environment.baseUrl;
+
+  hubConnection: HubConnection;
+  hubUrl = environment.hubUrl;
+  private messageThreadSource=new BehaviorSubject<string[]>([]);
+  messages$ = this.messageThreadSource.asObservable();
+
 
   constructor(private http: HttpClient) { }
   addMessage(message: Message) {
@@ -28,5 +37,23 @@ export class MessageService {
   }
   deleteMessage(id: number) {
     return this.http.delete(this.baseUrl + 'messages/' + id);
+  }
+  createHubConnection(user: User,otherUsername:string) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'message', { accessTokenFactory: () => user.token })
+      .withAutomaticReconnect()
+      .build();
+      this.hubConnection.start().catch(error=>{
+        console.log(error);
+      });
+      this.hubConnection.on('ReceiveMessageThread',messages=>{
+        this.messageThreadSource.next(messages);
+      });
+     
+  }
+  stopHubConnection(){
+    this.hubConnection.stop().catch(error=>{
+      console.log(error);
+    })
   }
 }
